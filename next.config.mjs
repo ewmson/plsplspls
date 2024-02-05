5a30016bece9d1ed0221d551c942a6eedee702aa
Eric Williamson <ericrw96@vt.edu>
2024-02-04 21:30:22 -0500

    no more setup pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: { unoptimized: true },
+  experimental: { images: { unoptimized: true } },
+  reactStrictMode: true,
+  swcMinify: true,
+  basePath: "/plsplspls",
 };
 
 export default nextConfig;
